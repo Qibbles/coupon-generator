f81a856e703b3c8078eeb15292c0df3d95d5181b
Gregory CHUA <coffeecup@gmail.com>
2020-02-20 12:05:21 +0800

    tabs added

--- coupon.py
+++ coupon.py
@@ -7,6 +7,19 @@ win = tk.Tk()
 win.title("Coupon HTML Creator")
 win.resizable(False, False)
 
-# Notebook
+## Notebook
 nb = ttk.Notebook(win)
-nb.grid(row=2, column=0, pady=(5,0))
+nb.grid(row=2, column=0, pady=(5,0), padx=(1,0), sticky='NSEW')
+
+# Input tab
+win = ttk.Frame(nb)
+nb.add(win, text="Main")
+
+# Help tab
+win = ttk.Frame(nb)
+nb.add(win, text="Help")
+
+helpMsg = "WIP"
+helpLabel = tk.Message(guide, text=helpMsg, width=500)
+helpLabel.grid(row=0, column=0)
+
