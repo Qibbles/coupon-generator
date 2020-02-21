92faf5bd2c30b5aa7f120f1b819bec2c0d302521
Gregory CHUA <coffeecup@gmail.com>
2020-02-21 19:21:39 +0800

    column 0 labels

--- coupon.py
+++ coupon.py
@@ -23,3 +23,11 @@ helpMsg = "WIP"
 helpLabel = tk.Message(guide, text=helpMsg, width=500)
 helpLabel.grid(row=0, column=0)
 
+coupon1Label = ttk.Label(win, text="Coupon 1")
+coupon1Label.grid(column=0, row=0)
+
+coupon2Label = ttk.Label(win, text="Coupon 2")
+coupon2Label.grid(column=0, row=1)
+
+coupon3Label = ttk.Label(win, text="Coupon 3")
+coupon3Label.grid(column=0, row=2)
